d33f1c09a3a9cb8280f0996b9c1cbacda30a4c82
Vladislav Chichuk <jake0_0@mail.ru>
2020-03-19 00:20:27 +0300

    sorry, one more time, before commit

--- src/Exception_In_Program/Exception_Program.java
+++ src/Exception_In_Program/Exception_Program.java
@@ -1,4 +1,17 @@
 package Exception_In_Program;
 
+import java.util.Scanner;
+
 public class Exception_Program {
+    public static void main(String[] args) {
+
+        System.out.println("program for converting cm in inch");
+        Scanner in = new Scanner(System.in);
+        final double CM_IN_INCH = 2.54;
+        System.out.println("Input a number");
+        Double NumberThatInput = in.nextDouble();
+        System.out.println("Result of converting = " + NumberThatInput * CM_IN_INCH);
+        in.close();
+
+    }
 }
--- src/learn_by_cours/initial_example.java
+++ src/learn_by_cours/initial_example.java
@@ -1,10 +1,22 @@
 package learn_by_cours;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+// wrong name of class, InitialExample better name for this class
 public class initial_example {
     public static void main(String[] args) {
-    System.out.println("hello JAVA");
-    System.out.println(19);
-    System.out.println("java " + 8.0);
-    System.out.println("\n\tjava " + 11.0);
+        System.out.println("program for converting cm in inch");
+        Scanner in = new Scanner(System.in);
+        final double CM_IN_INCH = 2.54 ;
+        System.out.println("Input a number");
+        Double NumberThatInput = in.nextDouble();
+        System.out.println("Result of converting = " + NumberThatInput * CM_IN_INCH );
+        in.close();
+
+
+
+
     }
+
 }
