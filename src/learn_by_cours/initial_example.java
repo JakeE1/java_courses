d92a0e9a921d6e0b4ecbd2ae1bde19f1353f66bb
Vladislav Chichuk <jake0_0@mail.ru>
2020-03-18 19:21:00 +0300

    created 1st class and add to repository

---
+++ src/learn_by_cours/initial_example.java
@@ -0,0 +1,10 @@
+package learn_by_cours;
+
+public class initial_example {
+    public static void main(String[] args) {
+    System.out.println("hello JAVA");
+    System.out.println(19);
+    System.out.println("java " + 8.0);
+    System.out.println("\n\tjava " + 11.0);
+    }
+}
